31bf58f179d14e65f8d8e1fad8b3ecbff2a7f9de
Bruno Damacena <damacena.bruno@gmail.com>
2021-07-11 09:54:39 -0300

    move cards.css to the assets folder and create the handout card

---
+++ public/vtt-tmp/assets/handout/handout.js
@@ -0,0 +1,5 @@
+const img_div = document.querySelector('#handout-picture');
+
+let img_url = '../images/rorik_ponte_de_pedra.png';
+
+img_div.innerHTML = `<img src="${img_url}" alt="Imagem do Handout">`;
