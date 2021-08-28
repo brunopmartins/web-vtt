db0681397658a1f4fce72a920efe06f95dbfd41c
Bruno Damacena <damacena.bruno@gmail.com>
2021-08-28 17:02:53 -0300

    update main app

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,23 @@
 import '../styles/globals.css'
-import '../styles/colmeia.css'
 import '../styles/home.css'
 // to do: remove global import
 import '../styles/playerSheet.css'
+import { QueryClient, QueryClientProvider } from 'react-query'
+
+ const queryClient = new QueryClient({
+   defaultOptions: {
+     queries: {
+       refetchOnWindowFocus: false,
+     },
+   },
+ })
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    return (
+        <QueryClientProvider client={queryClient}>
+            <Component {...pageProps} />
+        </QueryClientProvider>
+    )
 }
 
 export default MyApp
