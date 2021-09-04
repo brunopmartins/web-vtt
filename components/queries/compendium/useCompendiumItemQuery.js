c93ef36fc3079695ef28a4d0873add9c7248922c
Gabriel Dutra <nesk.frz@gmail.com>
2021-09-04 00:29:34 -0300

    Install axios

--- components/queries/compendium/useCompendiumItemQuery.js
+++ components/queries/compendium/useCompendiumItemQuery.js
@@ -1,10 +1,11 @@
+import axios from "axios";
 import { useQuery } from "react-query";
 const url = "https://www.dnd5eapi.co/api";
 
 export function useCompendiumItemQuery(category, item) {
   return useQuery(
     ["compendium", category, item],
-    () => fetch(`${url}/${category}/${item}`).then((res) => res.json()),
+    () => axios.get(`${url}/${category}/${item}`).then((res) => res.data),
     { enabled: !!item }
   );
 }
--- components/queries/compendium/useCompendiumListQuery.js
+++ components/queries/compendium/useCompendiumListQuery.js
@@ -1,8 +1,9 @@
+import axios from "axios";
 import { useQuery } from "react-query";
 const url = "https://www.dnd5eapi.co/api";
 
 export function useCompendiumListQuery(category) {
   return useQuery(["compendium", category], () =>
-    fetch(`${url}/${category}`).then((res) => res.json())
+    axios.get(`${url}/${category}`).then((res) => res.data)
   );
 }
