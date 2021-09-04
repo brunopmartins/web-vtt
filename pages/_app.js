406819b09863f09b655f3399e230af5567ea39f8
Bruno Damacena <damacena.bruno@gmail.com>
2021-09-03 23:14:49 -0300

    temporary import playerSheet css globally

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,8 @@ import "../styles/globals.css";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { ConfigProvider } from "antd";
 import ptBR from "antd/lib/locale/pt_BR";
+// to do: remove global import
+import '../styles/playerSheet.css'
 
 const queryClient = new QueryClient({
   defaultOptions: {
